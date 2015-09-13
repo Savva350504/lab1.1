91d093cb6e7648ec534d3cf32f3ddba8a4fe2857
Andrey <savich95@gmail.com>
2015-09-13 16:11:55 +0200

    Initial commit

---
+++ Complex.java
@@ -0,0 +1,55 @@
+
+public class Complex {
+
+    private double re;   // the real part
+    private double im;   // the imaginary part
+
+    /**
+     * create a new object with the given real and imaginary parts
+     *
+     * @param real a complex number real part
+     * @param imag a complex number imaginary part
+     */
+    public Complex(double real, double imag) {
+        re = real;
+        im = imag;
+    }
+
+    /**
+     * Add operation.
+     * @param b summand
+     * @return this Complex object whose value is (this + b)
+     */
+    public Complex plus(Complex b) {
+        re += b.re;
+        im += b.im;
+        return this;
+    }
+    public Complex minus(Complex b) {
+        re -= b.re;
+        im -= b.im;
+        return this;
+    }
+    /**
+     * Multiply operation.
+     * @param  b multiplier
+     * @return this Complex object whose value is this * b
+     */
+    public Complex times(Complex b) {
+        Complex a = this;
+        double real = a.re * b.re - a.im * b.im;
+        double imag = a.re * b.im + a.im * b.re;
+        re = real;
+        im = imag;
+        return this;
+    }
+
+    /**
+     * Square of Complex object's length, we're using square of length to
+     * eliminate the computation of square root
+     * @return square of length
+     */
+    public double lengthSQ() {
+        return re * re + im * im;
+    }
+}
---
+++ OffscreenImage.java
@@ -0,0 +1,31 @@
+
+/**
+ * Offscreen image produced by multi-threaded rendering engine.
+ * <p><i>
+ * This source code is provided to illustrate the usage of a given feature
+ * or technique and has been deliberately simplified. Additional steps
+ * required for a production-quality application, such as security checks,
+ * input validation and proper error handling, might not be present in
+ * this sample code.</i>
+ * @author akouznet
+ */
+public class OffscreenImage {
+    private final int width;
+    private final int height;
+
+    public OffscreenImage(int width, int height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+
+
+}
